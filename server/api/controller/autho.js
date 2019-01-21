46f7c1f2da757119ab3ba79c3adcbd6c794bcc9d
qq865738120 <zwj865738120@163.com>
2019-01-21 18:02:51 +0800

    dev

--- server/api/controller/autho.js
+++ server/api/controller/autho.js
@@ -30,6 +30,15 @@ module.exports = {
   * @apiGroup User
   * @apiParam {string} username 用户名
   * @apiParam {string} passwd 密码
+  * @apiSuccess {Number} code 错误码 200：登录成功；201：用户名或者密码不正确；300：传参异常
+  * @apiSuccess {String} msg 错误信息
+  * @apiSuccessExample {json} Success:
+  *{
+  *  code: 200,
+  *  msg: '登陆成功'
+  *}
+  * @apiErrorExample Error:
+  * { code: 300, msg: '密码参数异常' }
   * @apiVersion 1.0.0
   */
   async login(req, res) {
