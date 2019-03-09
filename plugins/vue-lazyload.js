94528084ac2cc01c0fd6710d619142f691f535a2
qq865738120 <zwj865738120@163.com>
2019-03-09 21:46:19 +0800

    dev

--- plugins/vue-lazyload.js
+++ plugins/vue-lazyload.js
@@ -4,6 +4,6 @@ import VueLazyload from 'vue-lazyload' //图片懒加载插件
 Vue.use(VueLazyload, {
   attempt: 1,
   lazyComponent: true,
-  error: 'http://q.img.soukong.cn/timg.jpg',
-  loading: 'http://q.img.soukong.cn/timg.jpg'
+  error: 'https://q.img.soukong.cn/timg.jpg',
+  loading: 'https://q.img.soukong.cn/timg.jpg'
 })
